7e14f94d46918c957b70c0c76c5ca8f939ae6499
evio <evio@vip.qq.com>
2019-03-02 18:07:28 +0800

    update

--- app/controller/index.js
+++ app/controller/index.js
@@ -1,13 +1,23 @@
 import {
   Http,
-  Controller
+  Controller,
+  Interface
 } from '@wox/wox';
 import IndexPage from '../webview/index.vue';
+import IndexService from '../service/index';
 
 @Controller
 export default class IndexController {
   @Http.Get
   async Home() {
-    await this.ctx.render(IndexPage);
+    const timestamp = await this.ctx.post('/timestamp');
+    await this.ctx.render(IndexPage, { timestamp });
+  }
+
+  @Http.Post('/timestamp')
+  @Interface.Service('index', IndexService)
+  async TimeStamp({ Service }) {
+    const body = this.ctx.req.body || {};
+    return await Service.index.getTime(body.delay);
   }
 }
