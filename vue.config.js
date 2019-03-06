13d22264bc61661edc4f2f4fa8d6c3cdf77b70d5
evio <evio@vip.qq.com>
2019-03-06 13:50:56 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,18 @@
 const WoxWebpackRuntimePlugin = require('@wox/loader');
+const path = require('path');
 module.exports = {
   chainWebpack(configs) {
     configs.entry('app').clear().add('./webpack.js');
     configs.plugin('wox').use(WoxWebpackRuntimePlugin);
     configs.resolve.alias.set('#', process.cwd());
+    configs.module.rule('js')
+      .exclude.clear()
+        .add(/@vue\/cli-service/)
+        .end()
+      .include
+        .add(path.resolve(__dirname, 'app'))
+        .add(/@wox/)
+        .end();
     return configs;
   }
 }
