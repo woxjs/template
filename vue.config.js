9628edc9124ff7a3668cca69ee2c706648874c91
evio <evio@vip.qq.com>
2019-03-09 21:11:34 +0800

    update wox.js and rewrite home template

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const WoxWebpackRuntimePlugin = require('@wox/loader');
+const webpackproxy = require('./webpack.proxy');
 module.exports = {
-  publicPath: '',
+  publicPath: '/',
   pages: {
     app: {
       entry: './webpack.js',
@@ -16,5 +17,14 @@ module.exports = {
     configs.plugin('wox').use(WoxWebpackRuntimePlugin);
     configs.resolve.alias.set('#', process.cwd());
     return configs;
+  },
+  devServer: {
+    historyApiFallback: true,
+    host: '0.0.0.0',
+    disableHostCheck: true
   }
+}
+
+if (Object.keys(webpackproxy).length) {
+  module.exports.devServer.proxy = webpackproxy;
 }
